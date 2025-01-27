72dac2ecda2aaf47d465e927fd369703792096b7
Bernhard <B.Jaeger1@gmx.net>
2025-01-27 19:55:11 +0100

    minor script change

--- setup_carla.sh
+++ setup_carla.sh
@@ -3,10 +3,10 @@
 # Download and install CARLA
 mkdir carla
 cd carla
-wget https://tiny.carla.org/carla-0-9-15-linux
+wget --content-disposition https://tiny.carla.org/carla-0-9-15-linux
 tar -xf CARLA_0.9.15.tar.gz
 cd Import
-wget https://tiny.carla.org/additional-maps-0-9-15-linux
+wget --content-disposition https://tiny.carla.org/additional-maps-0-9-15-linux
 cd ..
 ./ImportAssets.sh
 rm CARLA_0.9.15.tar.gz
